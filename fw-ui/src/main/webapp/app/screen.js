96cfd152cdffa2a62a3446985cd6fe0cb30ae4e6
murph141 <murph141@purdue.edu>
2015-03-27 12:51:17 -0400

    Starting to encorporate EditPage example

--- fw-ui/src/main/webapp/app/screen.js
+++ fw-ui/src/main/webapp/app/screen.js
@@ -10,16 +10,54 @@
     'use strict';
 
     //framework APIs
-    var f = api.fn,     //general API
-        def = api.def,  //application definition API
-        v = api.view;   //view API
+    var fn_api = api.fn,       //general API
+        def_api = api.def,     //application definition API
+        view_api = api.view;   //view API
 
-    f.trace('including screen.js');
+    // snap-in libs
+    var tags_api = api.lib.htmlTags,
+        widget_api = api.lib.widgetFactory;
 
-    function load(view) {}
+    fn_api.trace('including screen.js');
 
-    def.addView('screen',
-            {
+    function dlg(view, titleKey, text) {
+            var dlgDiv = tags_api.div( {
+                    attr: {
+                            id: view.mkId('dlg'),
+                            title: view.lion(titleKey)
+                    }
+            }).append(tags_api.p(text)),
+            $dlg = dlgDiv.domFrag(),
+            actions = {};
+
+            actions[view.lion('close')] = function () {
+                    $(this).dialog("close").dialog('destory');
+            };
+
+            $dlg.dialog({
+                    width: 600,
+                    buttons: actions
+            });
+
+    }
+
+    // Load a view
+    function load(view) {
+            var lion = view.lion;
+
+
+            view_api.setToolbar(
+                            widget_api.button({
+                                    text: lion('tbDeploy'),
+                                    click: function () {
+                                            dlg(view, 'deployDlg', 'This is where...');
+                                    }
+                            })
+            );
+    }
+
+    // Add the empty view
+    def_api.addView('screen', {
                     load: load
             });
 
