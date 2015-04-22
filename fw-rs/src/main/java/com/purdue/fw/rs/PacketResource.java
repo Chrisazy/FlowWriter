05e2b8a51e958b1dee585fb4a46c7a4efdb9608f
Christopher Wirt <chrisaz@gmail.com>
2015-04-22 09:29:25 -0400

    add

--- fw-rs/src/main/java/com/purdue/fw/rs/PacketResource.java
+++ fw-rs/src/main/java/com/purdue/fw/rs/PacketResource.java
@@ -26,10 +26,10 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import com.purdue.fw.model.Packet;
-import com.purdue.fw.api.InvalidInputException;
 import com.purdue.fw.api.PacketService;
 
 import com.hp.api.Id;
+import org.w3c.dom.Entity;
 
 /**
  * Sample Packet REST API resource.
@@ -37,6 +37,16 @@ import com.hp.api.Id;
 @Path("capture")
 public class PacketResource extends ControllerResource {
 
+    /**
+     * Creates a new Packet and registers it.
+     * <p>
+     * Normal Response Code(s): ok (200)
+     * <p>
+     * Error Response Codes: badRequest (400), unauthorized (401), forbidden (403),
+     * badMethod (405), serviceUnavailable (503)
+     *
+     * @param request JSON representation of a Packet to be created
+     * @return JSON object*/
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     public Response start(String request) {
@@ -44,17 +54,17 @@ public class PacketResource extends ControllerResource {
         ObjectMapper mapper = new ObjectMapper();
         
         JsonNode root = parse(mapper, request, "Packet data");
-        String ip_src = root.get("ip1").asText();
-        String ip_dst = root.get("ip2").asText();
-        int src_port = root.get("port1").asInt();
-        int dst_port = root.get("port2").asInt();
+        String ip_src =  root.get("ip_src") != null ? root.get("ip_src").asText() : null;
+        String ip_dst = root.get("ip_dst") != null ? root.get("ip_dst").asText() : null;
+        int port_src = root.get("port_src") != null ? root.get("port_src").asInt() : 0;
+        int port_dst = root.get("port_dst") != null ? root.get("port_dst").asInt() : 0;
         try {
-			svc.createAndSendMod(ip_src, ip_dst, src_port, dst_port);
-		} catch (InvalidInputException e) {
-			return null;
-		} catch (OpenflowException e) {
-			return null;
+			svc.createAndSendMod(ip_src, ip_dst, port_src, port_dst);
+		} catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST).
+                    entity(e.getMessage()).type("text/plain").build();
 		}
+
         return ok().build();
     }
     /*
